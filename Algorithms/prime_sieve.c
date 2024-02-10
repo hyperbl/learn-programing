95cbb689cefa1b2271efe1954ea1804f79090423
hyperbl <1741370745@qq.com>
2024-02-10 12:01:26 +0800

    add sieve of Atkin

--- Algorithms/prime_sieve.c
+++ Algorithms/prime_sieve.c
@@ -17,6 +17,9 @@ void segmented_sieve(int num, int arr[]);
 // 增量筛法
 void incremental_sieve(int num, int arr[]);
 
+// *阿特金筛法
+void atkin_sieve(int num, int arr[]);
+
 int main()
 {
     int num;
@@ -31,7 +34,8 @@ int main()
     // sundaram_sieve(num, array);
     // euler_sieve(num, array);
     // segmented_sieve(num, array);
-    incremental_sieve(num, array);
+    // incremental_sieve(num, array);
+    atkin_sieve(num, array);
     for (int i = 0; i < num; i++)
         if (array[i])
             printf("%d\n", i + 1);
@@ -101,4 +105,30 @@ void incremental_sieve(int num, int arr[])
             if (i % j == 0)
                 for (k = i; k <= num; k += j)
                     arr[k - 1] = 0;
+}
+
+void atkin_sieve(int num, int arr[])
+{
+    int i, j, k;
+    for (i = 4; i <= num; i++)
+        arr[i - 1] = 0;
+    for (i = 1; i * i <= num; i++)
+    {
+        for (j = 1; j * j <= num; j++)
+        {
+            k = 4 * i * i + j * j;
+            if (k <= num && (k % 12 == 1 || k % 12 == 5))
+                arr[k - 1] = !arr[k - 1];
+            k = 3 * i * i + j * j;
+            if (k <= num && k % 12 == 7)
+                arr[k - 1] = !arr[k - 1];
+            k = 3 * i * i - j * j;
+            if (i > j && k <= num && k % 12 == 11)
+                arr[k - 1] = !arr[k - 1];    
+        }
+    }
+    for (i = 5; i * i <= num; i++)
+        if (arr[i - 1])
+            for (j = i * i; j <= num; j += i * i)
+                arr[j - 1] = 0;
 }
