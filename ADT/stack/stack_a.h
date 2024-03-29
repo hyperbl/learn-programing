3c74ca8d2255cb00fae958f15a7149a4ca65d587
hyperbl <1741370745@qq.com>
2024-03-29 15:17:16 +0800

    修改了错别字

--- ADT/stack/stack_a.h
+++ ADT/stack/stack_a.h
@@ -18,7 +18,7 @@ int IsFull(Stack S);
 Stack CreateStack(int MaxElements);
 void DisposeStack(Stack S);
 void MakeEmpty(Stack S);
-void Push(ElementType X, Sack S);
+void Push(ElementType X, Stack S);
 ElementType Top(Stack S);
 void Pop(Stack S);
 ElementType TopAndPop(Stack S);
